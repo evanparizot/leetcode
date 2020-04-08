f34b41c59e35c53a98b4dbd7e7815301ff72bb55
eparrottalk <eparizot@gmail.com>
2020-04-07 21:09:04 -0400

    Add day 7

---
+++ day07.py
@@ -0,0 +1,14 @@
+
+from typing import List
+def countElements(arr: List[int]) -> int:
+  lookup = set(arr)
+  result = 0
+  for num in arr:
+    if (num + 1) in lookup:
+      result += 1
+
+  return result
+
+print(countElements([1,2,3])) # 2
+print(countElements([1,1,3,3,5,5,7,7])) # 0
+print(countElements([1,3,2,3,5,0])) # 3
